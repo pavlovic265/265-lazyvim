e305115044a72a10e86ceceb6ad7c4737a8b0a96
Marko Pavlovic <pavlovic265@gmail.com>
2024-12-13 14:36:29 +0100

    update

--- lua/plugins/neotest-jest.lua
+++ lua/plugins/neotest-jest.lua
@@ -1,9 +1,21 @@
 return {}
 -- return {
---   { "nvim-neotest/neotest-jest" },
---   {
---     "nvim-neotest/neotest",
---     opts = { adapters = { "neotest-plenary" } },
---     -- opts = { adapters = { "neotest-jest" } },
+--   "nvim-neotest/neotest",
+--   dependencies = {
+--     "nvim-neotest/neotest-jest",
 --   },
+--   config = function()
+--     require("neotest").setup({
+--       adapters = {
+--         require("neotest-jest")({
+--           jestCommand = "npm test --",
+--           jestConfigFile = "jest.config.ts",
+--           env = { CI = true },
+--           cwd = function(path)
+--             return vim.fn.getcwd()
+--           end,
+--         }),
+--       },
+--     })
+--   end,
 -- }
